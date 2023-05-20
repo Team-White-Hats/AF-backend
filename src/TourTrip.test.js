6ee807608f241030868d0a028599cd7b5c1e664b
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-05-20 12:52:25 +0530

    make changes in login

--- src/TourTrip.test.js
+++ src/TourTrip.test.js
@@ -1,58 +1,58 @@
 const request = require("supertest")
 const baseURL = "http://localhost:8000";
 
-// describe(`GET ${baseURL}/api/tourtrip/all`, () => {
-//     const newTourTrip = {
-//         placeName: "Kandy",
-//         startLocation: "Colombo",
-//         endLocation: "Kandy",
-//         transportType: "Van",
-//         description: "Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest.",
-//         entryPrice: "500",
-//         products: "Handicrafts",
-//         productImages: "https://media.timeout.com/images/101852777/380/285/image.jpg",
-//         statusType: "Available",
-//         route: "123",
-//     }
-//     it("should return 200 OK", async () => {
-//         const response = await request(baseURL).get("/api/tourtrip/all");
-//         expect(response.statusCode).toBe(200);
-//     });
-//     it("should return data", async () => {
-//         const response = await request(baseURL).get(`/api/tourtrip/all`);
-//         expect(response.body.length >= 1).toBe(true);
-//     });
-// });
+describe(`GET ${baseURL}/api/tourtrip/all`, () => {
+    const newTourTrip = {
+        placeName: "Kandy",
+        startLocation: "Colombo",
+        endLocation: "Kandy",
+        transportType: "Van",
+        description: "Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest.",
+        entryPrice: "500",
+        products: "Handicrafts",
+        productImages: "https://media.timeout.com/images/101852777/380/285/image.jpg",
+        statusType: "Available",
+        route: "123",
+    }
+    it("should return 200 OK", async () => {
+        const response = await request(baseURL).get("/api/tourtrip/all");
+        expect(response.statusCode).toBe(200);
+    });
+    it("should return data", async () => {
+        const response = await request(baseURL).get(`/api/tourtrip/all`);
+        expect(response.body.length >= 1).toBe(true);
+    });
+});
 
-// describe(`POST ${baseURL}/api/tourtrip/create`, () => {
-//     const newTourTrip = {
-//         placeName: "Kandy",
-//         startLocation: "Colombo",
-//         endLocation: "Kandy",
-//         transportType: "Van",
-//         description: "Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest.",
-//         entryPrice: "500",
-//         products: "Handicrafts",
-//         productImages: "https://media.timeout.com/images/101852777/380/285/image.jpg",
-//         statusType: "Available",
-//         route: "123",
-//             }
-//     it("should add Tour Trip", async () => {
-//         const response = await request(baseURL).post("/api/tourtrip/create").send(newTourTrip);
-//         expect(response.statusCode).toBe(201);
-//         expect(response.body.placeName).toBe(newTourTrip.placeName);
-//         expect(response.body.startLocation).toBe(newTourTrip.startLocation);
-//         expect(response.body.endLocation).toBe(newTourTrip.endLocation);
-//         expect(response.body.transportType).toBe(newTourTrip.transportType);
-//         expect(response.body.description).toBe(newTourTrip.description);
-//         expect(response.body.entryPrice).toBe(newTourTrip.entryPrice);
-//         expect(response.body.products).toBe(newTourTrip.products);
-//         expect(response.body.productImages).toBe(newTourTrip.productImages);
-//         expect(response.body.statusType).toBe(newTourTrip.statusType);
-//         expect(response.body.route).toBe(newTourTrip.route);
-//     })
+describe(`POST ${baseURL}/api/tourtrip/create`, () => {
+    const newTourTrip = {
+        placeName: "Kandy",
+        startLocation: "Colombo",
+        endLocation: "Kandy",
+        transportType: "Van",
+        description: "Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest.",
+        entryPrice: "500",
+        products: "Handicrafts",
+        productImages: "https://media.timeout.com/images/101852777/380/285/image.jpg",
+        statusType: "Available",
+        route: "123",
+            }
+    it("should add Tour Trip", async () => {
+        const response = await request(baseURL).post("/api/tourtrip/create").send(newTourTrip);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.placeName).toBe(newTourTrip.placeName);
+        expect(response.body.startLocation).toBe(newTourTrip.startLocation);
+        expect(response.body.endLocation).toBe(newTourTrip.endLocation);
+        expect(response.body.transportType).toBe(newTourTrip.transportType);
+        expect(response.body.description).toBe(newTourTrip.description);
+        expect(response.body.entryPrice).toBe(newTourTrip.entryPrice);
+        expect(response.body.products).toBe(newTourTrip.products);
+        expect(response.body.productImages).toBe(newTourTrip.productImages);
+        expect(response.body.statusType).toBe(newTourTrip.statusType);
+        expect(response.body.route).toBe(newTourTrip.route);
+    })
 
-// });
+});
 
 describe(`DELETE ${baseURL}/api/tourtrip/delete`, () => {
     const newTourTrip = {
--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -8,125 +8,128 @@ const LocalStorage = require("node-localstorage").LocalStorage;
 
 var localStorage = new LocalStorage("./scratch");
 
-const login = async(req, res) => {
-    try {
-        //validate the user input fields
-        const { error } = LoginValidation(req.body);
-        if(error) {
-            res.send({ message: error["details"][0]["message"] });
-        }
+const login = async (req, res) => {
+	try {
+		//validate the user input fields
+		const { error } = LoginValidation(req.body);
+		if (error) {
+			res.send({ message: error["details"][0]["message"] });
+		}
 
-        // taking data from the database for relavent username
-        const userData = await user.findOne({
-            email: req.body.email,
-        }); // if it is a user
-        const adminData = await admin.findOne({
-            email: req.body.email,
-        }); // if it is a admin
+		// taking data from the database for relavent username
+		const userData = await user.findOne({
+			email: req.body.email,
+		}); // if it is a user
+		const adminData = await admin.findOne({
+			email: req.body.email,
+		}); // if it is a admin
 
-        if(userData) {
-            // code fot testing purposes
+		console.log("userData", userData);
 
-            localStorage.setItem("isUser", userData.isUser);
-            console.log("===================");
-            console.log("Logged as a User");
-            console.log("===================");
+		if (userData) {
+			// code fot testing purposes
 
-            // decypting the user password
-            const decryptedPassword = CryptoJS.AES.decrypt(
-                userData.password,
-                process.env.PASS_SECRET,
-            ).toString(CryptoJS.enc.Utf8);
+			localStorage.setItem("isUser", userData.isUser);
 
-            try {
-                // checking whether the password we entered is correct or not
-                if(decryptedPassword !== req.body.password) {
-                    res.status(401).json("Wrong Password !");
-                    console.log("Wrong Password!");
-                } else {
-                    console.log(userData);
-                    console.log("Login Successfull !");
-                    // generating the web token to the specified user
-                    const accessToken = JWT.sign({
-                            _id: userData.id,
-                        },
-                        process.env.JWT_SECRET, { expiresIn: "3d" },
-                    );
+			// decypting the user password
+			const decryptedPassword = CryptoJS.AES.decrypt(
+				userData.password,
+				process.env.PASS_SECRET,
+			).toString(CryptoJS.enc.Utf8);
 
-                    res.header("authentication", accessToken).send({
-                        authentication: accessToken,
-                        role: "user",
-                        roleData: userData,
-                    });
+			try {
+				// checking whether the password we entered is correct or not
+				if (decryptedPassword !== req.body.password) {
+					res.status(401).json("Wrong Password !");
+					console.log("Wrong Password!");
+				} else {
+					console.log(userData);
+					console.log("Login Successfull !");
+					// generating the web token to the specified user
+					const accessToken = JWT.sign(
+						{
+							_id: userData.id,
+						},
+						process.env.JWT_SECRET,
+						{ expiresIn: "3d" },
+					);
 
-                    // if the user exist : show data in the user variable
-                    res.status(200).json({ accessToken });
-                }
-            } catch(err) {
-                res.status(400).json(err.message); // error handling
-            }
-        } else if(adminData) {
-            localStorage.setItem("isAdmmin", adminData.isAdmin);
-            console.log("=========================");
-            console.log("Logged as a Admin");
-            console.log("=========================");
+					const json = {
+						authentication: accessToken,
+						role: "user",
+						roleData: userData,
+					};
 
-            // decypting the patient password
-            const decryptedPassword = CryptoJS.AES.decrypt(
-                adminData.password,
-                process.env.PASS_SECRET,
-            ).toString(CryptoJS.enc.Utf8);
+					// if the user exist : show data in the user variable
+					res.status(200).json(json);
+				}
+			} catch (err) {
+				res.status(400).json(err.message); // error handling
+			}
+		} else if (adminData) {
+			localStorage.setItem("isAdmmin", adminData.isAdmin);
 
-            try {
-                // checking whether the password we entered is correct or not
-                if(decryptedPassword !== req.body.password) {
-                    return res.status(401).json("Wrong Password !");
-                } else {
-                    // Testing Purpose
-                    console.log(adminData);
-                    console.log("Login Successfull !");
+			// decypting the patient password
+			const decryptedPassword = CryptoJS.AES.decrypt(
+				adminData.password,
+				process.env.PASS_SECRET,
+			).toString(CryptoJS.enc.Utf8);
 
-                    // generating the web token to the specified user
-                    const accessToken = JWT.sign({
-                            _id: adminData.id,
-                        },
-                        process.env.JWT_SECRET, { expiresIn: "3d" },
-                    );
+			try {
+				// checking whether the password we entered is correct or not
+				if (decryptedPassword !== req.body.password) {
+					return res.status(401).json("Wrong Password !");
+				} else {
+					// Testing Purpose
+					console.log(adminData);
+					console.log("Login Successfull !");
 
-                    res.header("authentication", accessToken).send({
-                        authentication: accessToken,
-                        role: "admin",
-                        roleData: adminData,
-                    });
+					// generating the web token to the specified user
+					const accessToken = JWT.sign(
+						{
+							_id: adminData.id,
+						},
+						process.env.JWT_SECRET,
+						{ expiresIn: "3d" },
+					);
 
-                    // if the user exist : show data in the user variable
-                    res.status(200).json({ accessToken });
-                }
-            } catch(err) {
-                res.status(400).json(err.message); // error handling
-            }
-        } else {
-            res.status(500).json("Wrong credentials !");
-        }
-    } catch(err) {}
+					res.header("authentication", accessToken).send({
+						authentication: accessToken,
+						role: "admin",
+						roleData: adminData,
+					});
+
+					// if the user exist : show data in the user variable
+					res.status(200).json({ accessToken });
+				}
+			} catch (err) {
+				res.status(400).json(err.message); // error handling
+			}
+		} else {
+			res.status(500).json("Wrong credentials !");
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(400).json(err.message);
+	}
 };
 
 let refreshTokens = [];
 
-const logout = async(req, res) => {
-    const TokenRefresh = req.params.authentication;
+const logout = async (req, res) => {
+	const TokenRefresh = req.params.authentication;
 
-    localStorage.clear();
+	localStorage.clear();
 
-    try {
-        refreshTokens = refreshTokens.filter(
-            (accessToken) => accessToken !== TokenRefresh,
-        );
-        res.status(200).json("You are logged out successfully");
-        console.log("You are logged out successfully !");
-    } catch(err) {
-        return res.status(400).send({ message: err });
-    }
+	try {
+		refreshTokens = refreshTokens.filter(
+			(accessToken) => accessToken !== TokenRefresh,
+		);
+		res.status(200).json("You are logged out successfully");
+		console.log("You are logged out successfully !");
+	} catch (err) {
+		return res.status(400).send({ message: err });
+	}
 };
 
 module.exports = { login, logout };
